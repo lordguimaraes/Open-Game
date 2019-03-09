59c9b25973dee79258a4adb586a0f9f4d410c8fe
ftarden <47696491+ftarden@users.noreply.github.com>
2019-03-09 19:37:28 -0300

    Add files via upload
    Nova versão com route.

---
+++ Open-Game/js/opengame.cadastro.controller.js
@@ -0,0 +1,5 @@
+angular.module('app').controller('cadastroCtrl', function($scope) {
+
+   $scope.logoMarca = [{id: 'logo' , nome:'logo - open game', descricao:'logo', imagemUrl: './images/logo.png' }]
+
+});
