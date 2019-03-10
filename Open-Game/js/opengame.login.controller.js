7a8f15890e5202a018d11c992d0b147a1a120575
ftarden <47696491+ftarden@users.noreply.github.com>
2019-03-10 18:20:19 -0300

    Add files via upload
    Falta alguns detalhes de sessão.

--- Open-Game/js/opengame.login.controller.js
+++ Open-Game/js/opengame.login.controller.js
@@ -1,14 +1,26 @@
 angular.module('app').controller('loginCtrl', function($scope, $firebaseAuth) {
 
+
 	var auth = $firebaseAuth();
 	
 	$scope.login = function (){
 		auth.$signInWithEmailAndPassword($scope.email, $scope.senha)
 		.then(function(firebaseUser){
 			console.log(firebaseUser)
+
 		}).catch(function(error){
 			console.log(error)
 		})
+
+	auth.$onAuthStateChanged(function(firebaseUser) {
+	  	
+	  	$scope.firebaseUser = firebaseUser;
+
+	});
+
+
 	}
+
+	
 	
 });
