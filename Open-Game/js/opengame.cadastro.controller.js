4e01b898b4fcade3b3180a8979814f85322ed22d
ftarden <47696491+ftarden@users.noreply.github.com>
2019-03-12 20:26:41 -0300

    Add files via upload
    Novo login.

--- Open-Game/js/opengame.cadastro.controller.js
+++ Open-Game/js/opengame.cadastro.controller.js
@@ -2,5 +2,42 @@ angular.module('app').controller( 'cadastroCtrl', ['$scope', '$state', '$firebas
 
 	 $scope.logoMarca = [{id: 'logo' , nome:'logo - open game', descricao:'logo', imagemUrl: './images/logo.png' }]
 
-	
+	 $scope.authObj = $firebaseAuth();
+	 $scope.email = '';
+	 $scope.senha = '';
+
+	 function sleep(ms) {
+	  return new Promise(resolve => setTimeout(resolve, ms));
+	}
+
+	 async function dormirSegundos() {
+  		
+ 	 await sleep(3000);
+ 		
+	}
+
+	$scope.cadastrar = function(){
+
+		console.log($scope.email)
+
+		$scope.authObj.$createUserWithEmailAndPassword($scope.email, $scope.senha)
+  		.then(function(firebaseUser) {
+
+  		  $scope.authObj.$signOut();
+
+  		  dormirSegundos();
+
+  		  $window.alert('== Usuário cadastrado com sucesso! Faça seu login! ==');
+
+  		  dormirSegundos();
+  
+  		  $state.go('home');
+  		  
+ 		 }).catch(function(error) {
+  		  console.error("Error: ", error);
+ 		 });
+
+
+	}
+
 }]);
