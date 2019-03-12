60b2341458d5a07defe42f90a9814345e2791cc9
ftarden <47696491+ftarden@users.noreply.github.com>
2019-03-11 21:24:21 -0300

    Add files via upload
    Melhorado o login.

--- Open-Game/js/opengame.route.js
+++ Open-Game/js/opengame.route.js
@@ -15,15 +15,11 @@ angular.module('app').config(function ($stateProvider, $urlRouterProvider){
 		templateUrl: "view/pedido.html",
 		controller: "pedidoCtrl",
 		resolve: {
-      // controller will not be loaded until $requireSignIn resolves
-      // Auth refers to our $firebaseAuth wrapper in the factory below
+
       "currentAuth": ["Auth", function(Auth) {
-        // $requireSignIn returns a promise so the resolve waits for it to complete
-        // If the promise is rejected, it will throw a $routeChangeError (see above)
         
         return Auth.$requireSignIn();
         
-
       }]
     }
 		
@@ -40,31 +36,9 @@ angular.module('app').config(function ($stateProvider, $urlRouterProvider){
 		controller: "empresaCtrl"
 		
 	});
-	$urlRouterProvider.otherwise('/')
-	
+	$urlRouterProvider.otherwise('/');
 
 });
-
-// angular.module('app').run(["$rootScope", "$location", function($rootScope, $location) {
-//   $rootScope.$on("$routeChangeError", function(event, next, previous, error) {
-//     // We can catch the error thrown when the $requireSignIn promise is rejected
-//     // and redirect the user back to the home page
-//     if (error === "AUTH_REQUIRED") {
-//       $location.path("/login");
-//     }
-//   });
-// }]);
-
-angular.module('app').run(["$rootScope", "$state", function($rootScope, $state) {
-  $rootScope.$on("$stateChangeError", function(event, toState, toParams, fromState, fromParams, error) {
-    // We can catch the error thrown when the $requireSignIn promise is rejected
-    // and redirect the user back to the home page
-    console.log('aqui')
-    if (error === "AUTH_REQUIRED") {
-      $state.go("/login");
-    }
-  });
-}]);
 
 angular.module('app').factory("Auth", ["$firebaseAuth",
   function($firebaseAuth) {
