f4598ee78dd4f907cfac1b4e122566bcbdf13741
ftarden <47696491+ftarden@users.noreply.github.com>
2019-03-13 20:26:40 -0300

    Add files via upload
    Pedido sendo enviado.

--- Open-Game/js/app.js
+++ Open-Game/js/app.js
@@ -1,11 +1,11 @@
-angular.module('app', ['ui.router', 'firebase']);
+angular.module('app', ['ui.router', 'firebase', 'ngFileUpload']);
 
 var config = {
     apiKey: "AIzaSyDeko7ZdGZhuWh8PsejcdXHUxH5eKjI50A",
     authDomain: "open-game-861b3.firebaseapp.com",
     databaseURL: "https://open-game-861b3.firebaseio.com",
     projectId: "open-game-861b3",
-    storageBucket: "",
+    storageBucket: "gs://open-game-861b3.appspot.com",
     messagingSenderId: "574595088527"
   };
   firebase.initializeApp(config);
